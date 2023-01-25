ee2db2944176a53eb18883b6f8db4d432740ee58
atikahas <45572226+atikahas@users.noreply.github.com>
2023-01-25 12:28:46 +0800

    update

--- resources/views/layouts/gallery-sub.blade.php
+++ resources/views/layouts/gallery-sub.blade.php
@@ -24,6 +24,9 @@
     @yield('scriptheader')
   </head>
   <body>
+    <div class="scroll-up-btn show">
+      <ion-icon name="arrow-up-outline"></ion-icon>
+    </div>
     
     <header class="site-header">
       <div class="container-fluid">
@@ -59,45 +62,11 @@
 
     <footer class="section footer-section">
       <div class="container">
-        {{-- <div class="row mb-4">
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Terms &amp; Conditions</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-              <li><a href="#">Help</a></li>
-             <li><a href="#">Rooms</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">Our Location</a></li>
-              <li><a href="#">The Hosts</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Restaurant</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5 pr-md-5 contact-info">
-            <p><span class="d-block">Address:</span> <span> 98 West 21th Street, Suite 721 New York NY 10016</span></p>
-            <p><span class="d-block">Phone:</span> <span> (+1) 435 3533</span></p>
-            <p><span class="d-block">Email:</span> <span> info@yourdomain.com</span></p>
-          </div>
-          <div class="col-md-3 mb-5">
-            <p>Sign up for our newsletter</p>
-            <form action="#" class="footer-newsletter">
-              <div class="form-group">
-                <input type="email" class="form-control" placeholder="Your email...">
-                <button type="submit" class="btn"><span class="fa fa-paper-plane"></span></button>
-              </div>
-            </form>
-          </div>
-        </div> --}}
         <div class="row ">
           <p class="col-md-6 text-left" data-aos="fade-up">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | TK</p>
             
           <p class="col-md-6 text-right social">
-            <img src="{{url('')}}/villa/agalogo/aga-logo-8.png" height="50px" width="50px" data-aos="fade-up"/>
+            <img src="{{url('')}}/villa/agalogo/aga-logo-8.png" height="50px" width="50px"/>
           </p>
         </div>
       </div>
@@ -109,6 +78,23 @@
     <script src="{{url('')}}/villa/js/owl.carousel.min.js"></script>
     <script src="{{url('')}}/villa/js/aos.js"></script>
     <script src="{{url('')}}/villa/js/main.js"></script>
+    <script>
+      $(document).ready(function () {
+        // $(window).scroll(function () {
+        //   if (this.scrollY > 100) {
+        //     $(".scroll-up-btn").addClass("show");
+        //   } else {
+        //     $(".scroll-up-btn").removeClass("show");
+        //   }
+        // });
+
+        $(".scroll-up-btn").click(function () {
+          $("html").animate({ scrollTop: 0 });
+          $("html").css("scrollBehavior", "auto");
+        });
+      });
+
+    </script>
     @yield('scriptfooter')
   </body>
 </html>
--- resources/views/layouts/gallery.blade.php
+++ resources/views/layouts/gallery.blade.php
@@ -37,7 +37,6 @@
         <div class="row site-hero-inner justify-content-center align-items-center">
           <div class="col-md-10 text-center">
             <img src="{{url('')}}/villa/agalogo/aga-logo-8.png" height="150px" width="150px" data-aos="fade-up"/>
-            {{-- <h1 class="heading" data-aos="fade-up">AGA <em>Villa</em></h1> --}}
             <p data-aos="fade-up" data-aos-delay="100" style="color:white">
               Immerse yourself in peace, serenity and chilluxe.
             </p>
@@ -56,45 +55,11 @@
 
     <footer class="section footer-section">
       <div class="container">
-        {{-- <div class="row mb-4">
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Terms &amp; Conditions</a></li>
-              <li><a href="#">Privacy Policy</a></li>
-              <li><a href="#">Help</a></li>
-             <li><a href="#">Rooms</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">Our Location</a></li>
-              <li><a href="#">The Hosts</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">Restaurant</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5 pr-md-5 contact-info">
-            <p><span class="d-block">Address:</span> <span> 98 West 21th Street, Suite 721 New York NY 10016</span></p>
-            <p><span class="d-block">Phone:</span> <span> (+1) 435 3533</span></p>
-            <p><span class="d-block">Email:</span> <span> info@yourdomain.com</span></p>
-          </div>
-          <div class="col-md-3 mb-5">
-            <p>Sign up for our newsletter</p>
-            <form action="#" class="footer-newsletter">
-              <div class="form-group">
-                <input type="email" class="form-control" placeholder="Your email...">
-                <button type="submit" class="btn"><span class="fa fa-paper-plane"></span></button>
-              </div>
-            </form>
-          </div>
-        </div> --}}
         <div class="row ">
           <p class="col-md-6 text-left" data-aos="fade-up">Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | TK</p>
             
           <p class="col-md-6 text-right social">
-            <img src="{{url('')}}/villa/agalogo/aga-logo-8.png" height="50px" width="50px" data-aos="fade-up"/>
+            <img src="{{url('')}}/villa/agalogo/aga-logo-8.png" height="50px" width="50px"/>
           </p>
         </div>
       </div>
